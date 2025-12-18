204d90bcdadba9ca596569c3ee3fadba5c507715
Alejandro Behar <89646656+alejandrobg101@users.noreply.github.com>
2025-12-18 14:39:57 -0400

    Change API base URL for local development

--- app/streamlitApp/api_config.py
+++ app/streamlitApp/api_config.py
@@ -1,4 +1,4 @@
 # Backend API URL
-API_BASE_URL = "https://project-jac-b441daed17a6.herokuapp.com/api"
+API_BASE_URL = "http://127.0.0.1:5000/api"
 # For local chatbot
 API_LOCAL_BASE_URL = "http://127.0.0.1:5000/api"
